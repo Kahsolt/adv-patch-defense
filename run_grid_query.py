066a6c7a16301b16204d644a1762b690c067c7f2
Kahsolt <kahsolt@qq.com>
2023-06-04 11:23:16 +0800

    add saliency map

--- run_grid_query.py
+++ run_grid_query.py
@@ -8,9 +8,11 @@ import pickle as pkl
 from pathlib import Path
 from time import time
 from argparse import ArgumentParser
+from typing import List
 
 from torch.nn import Module
 import torch.nn.functional as F
+from torch.autograd import grad
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
@@ -23,19 +25,27 @@ if 'repos':
 
 from utils import *
 
-BASE_PATH = Path(__file__).parent.absolute()
+BASE_PATH = Path(__file__).parent
 LOG_PATH = BASE_PATH / 'log' ; LOG_PATH.mkdir(exist_ok=True)
 
 
-@torch.inference_mode()
-def run(args, model:Module, X:Tensor, Y:Tensor, P:Tensor):
+def saliency_map(model:Module, X:Tensor, Y:Tensor) -> np.ndarray:
+  assert X.shape[0] == len(Y) == 1
+  X.requires_grad = True
+  logits = model(normalize(X))
+  loss = F.cross_entropy(logits, Y, reduction='none')
+  g = grad(loss, X, loss)[0]      # [B, H, W]
+  g = g.abs().max(dim=1).values   # [B, C, H, W]
+  return g[0].cpu().numpy().astype(np.float16)
+
+def grid_query(model:Module, X:Tensor, Y:Tensor, P:Tensor) -> List[np.ndarray]:
   B, C, H, W = X.shape ; assert B == 1
   pB, pC, pH, pW = P.shape ; assert C == pC
   shape = qH, qW = [H - pH + 1, W - pW + 1]
-  logits = np.zeros(shape, dtype=np.float32)
-  probs  = np.zeros(shape, dtype=np.float32)
-  losses = np.zeros(shape, dtype=np.float32)
-  preds  = np.zeros(shape, dtype=np.int32)
+  logits = np.zeros(shape, dtype=np.float16)
+  probs  = np.zeros(shape, dtype=np.float16)
+  losses = np.zeros(shape, dtype=np.float16)
+  preds  = np.zeros(shape, dtype=np.uint16)
 
   total = qH * qW
   n_batch = total // args.batch_size
@@ -53,10 +63,10 @@ def run(args, model:Module, X:Tensor, Y:Tensor, P:Tensor):
     pred  = logit.argmax(dim=-1)
 
     for idx, (i, j) in enumerate(batch_idx):
-      logits [i, j] = logit[idx][y].item()
-      probs  [i, j] = prob [idx][y].item()
-      losses [i, j] = loss [idx]   .item()
-      preds  [i, j] = pred [idx]   .item()
+      logits[i, j] = logit[idx][y].item()
+      probs [i, j] = prob [idx][y].item()
+      losses[i, j] = loss [idx]   .item()
+      preds [i, j] = pred [idx]   .item()
 
     batch_AX .clear()
     batch_idx.clear()
@@ -78,32 +88,61 @@ def run(args, model:Module, X:Tensor, Y:Tensor, P:Tensor):
     pbar.update()
   pbar.close()
 
-  expanme = f'grid_query-img={args.idx}_ptc={args.ip_idx}_r={args.ratio}'
-  
-  plt.subplot(221) ; plt.title('logits')  ; sns.heatmap(logits)
-  plt.subplot(222) ; plt.title('probs')   ; sns.heatmap(probs, vmin=0.0, vmax=1.0)
-  plt.subplot(223) ; plt.title('losses')  ; sns.heatmap(losses)
-  plt.subplot(224) ; plt.title('correct') ; sns.heatmap(preds, vmin=0, vmax=999)
-  plt.savefig(LOG_PATH / f'{expanme}.png', dpi=600)
+  return logits, probs , losses, preds
 
-  with lzma.open(LOG_PATH / f'{expanme}.xz.pkl', 'wb') as fh:
+
+@torch.no_grad()
+def run(args, model:Module, X:Tensor, Y:Tensor, P:Tensor):
+  with torch.inference_mode():
+    # raw pred
+    y: int = Y[0].item()
+    y_hat: int = model(normalize(X)).argmax(dim=-1)[0].item()
+    print(f'>> truth: {y}, raw_pred: {y_hat}')
+
+    # grid query
+    logits, probs, losses, preds = grid_query(model, X, Y, P)
+    correct: np.ndarray = preds == y
+    asr: float = 1.0 - correct.sum() / correct.size
+    print(f'>> random asr: {asr:.3%}')
+
+  with torch.enable_grad():
+    # saliency map
+    smap = saliency_map(model, X, Y)
+
+  expanme = f'grid_query-{args.model}_img={args.idx}_ptc={args.ip_idx}_r={args.ratio}'
+  suptitle = f'{args.model}_img={args.idx}_ptc={args.ip_idx}_r={args.ratio} (asr: {asr:.3%})'
+
+  plt.figure(figsize=(16, 10))
+  plt.subplot(231) ; plt.title('logits')   ; sns.heatmap(logits)
+  plt.subplot(234) ; plt.title('probs')    ; sns.heatmap(probs, vmin=0.0, vmax=1.0)
+  plt.subplot(232) ; plt.title('losses')   ; sns.heatmap(losses)
+  plt.subplot(235) ; plt.title('saliency') ; sns.heatmap(smap)
+  plt.subplot(233) ; plt.title('preds')    ; sns.heatmap(preds, vmin=0, vmax=999)
+  plt.subplot(236) ; plt.title('correct')  ; sns.heatmap(correct, vmin=0, vmax=1)
+  plt.suptitle(suptitle)
+  plt.tight_layout()
+  fp = args.out_path / f'{expanme}.png'
+  plt.savefig(fp, dpi=600)
+  print(f'>> save figure to {fp}')
+
+  fp = args.out_path / f'{expanme}.xz.pkl'
+  print(f'>> save results to {fp}')
+  with lzma.open(fp, 'wb') as fh:
     pkl.dump({
-      'losses': losses,
+      'truth':  y,
+      'pred':   y_hat,
       'logits': logits,
       'probs':  probs,
+      'losses': losses,
       'preds':  preds,
+      'smap':   smap,
+      'asr':    asr,
     }, fh)
 
 
 def go(args):
   model = get_model(args.model).to(device)
   model.eval()
-
-  dataloader = get_dataloader(bs=1)
-  for i, (X, Y) in enumerate(dataloader):
-    if i >= args.idx: break
-  X = X.to(device)
-  Y = Y.to(device)
 
   with gzip.open(PACTCH_FILE, 'rb') as f:
     patches, targets, info = pkl.load(f)
@@ -115,18 +154,31 @@ def go(args):
   scale: float = 224 * args.scale / patch_size
   P = F.interpolate(patch, scale_factor=scale, mode='nearest')
 
-  run(args, model, X, Y, P)
+  dataloader = get_dataloader(bs=1)
+  if args.idx_all:
+    for i, (X, Y) in enumerate(dataloader):
+      print(f'[{i} / {len(dataloader)}]')
+      X = X.to(device)
+      Y = Y.to(device)
+      run(args, model, X, Y, P)
+  else:
+    for i, (X, Y) in enumerate(dataloader):
+      if i >= args.idx: break
+    X = X.to(device)
+    Y = Y.to(device)
+    run(args, model, X, Y, P)
 
 
 if __name__ == '__main__':
   parser = ArgumentParser()
-  # model & data
   parser.add_argument('-M', '--model', default='resnet50', choices=MODELS, help='model to attack')
-  parser.add_argument('--batch_size', default=512, type=int)
+  parser.add_argument('--batch_size', default=512, type=int, help='patch attack query batch size')
   parser.add_argument('--idx', default=0, type=int, help='run image sample index')
-  # ImageNet-patch (attack)
+  parser.add_argument('--idx_all', action='store_true', help='run all test images')
   parser.add_argument('--ratio', default=0.05, type=float, help='attack patch area ratio, typically 1%~5%')
   parser.add_argument('--ip_idx', default=0,   type=int,   help='which pre-gen patch to use (index range 0 ~ 9)')
+  parser.add_argument('--ip_idx_all', action='store_true', help='run all pre-generated patches')
+  parser.add_argument('--out_path', default=LOG_PATH, type=Path, help='out data path')
   args = parser.parse_args()
 
   assert 0.0 < args.ratio < 1.0
